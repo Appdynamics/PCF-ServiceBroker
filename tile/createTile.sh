c149b46a8a25726c1722ac9ea46e097095d57134
Nikhil Katre <nkatre@ncsu.edu>
2016-05-31 12:58:28 -0700

    db with analytics agent

--- tile/createTile.sh
+++ tile/createTile.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-#PCF_VERSION=1.4
-TILE_VERSION=4.1.5.2.0
+TILE_VERSION=v1.1-OpsMgrv1.6-MAv4.1.5.3 # change
 TILE_NAME=AppDynamics-Broker
-TILE_FILE=`pwd`/*tile.yml
+TILE_FILE=`pwd`/appdynamics-broker-tile-v1.6.yml
 RELEASE_TARFILE=`pwd`/releases/*/*.tgz
+
+CONTENT_MIGRATIONS=`pwd`/content_migrations
+
 
 #BOSH_STEMCELL_FILE=`cat ${TILE_FILE} | grep "bosh-stemcell" | grep "^ *file:" | awk '{print $2}' `
 #BOSH_STEMCELL_LOCATION=https://s3.amazonaws.com/bosh-jenkins-artifacts/bosh-stemcell/vsphere
@@ -16,12 +18,14 @@ mkdir -p metadata releases #stemcells
 cp $TILE_FILE metadata
 cp $RELEASE_TARFILE releases
 
+cp -r ${CONTENT_MIGRATIONS} .
+
 # Ignore bundling the stemcell as most often the Ops Mgr carries the stemcell.
 # If Ops Mgr complains of missing stemcell, change the version specified inside the tile to the one that Ops mgr knows about
 
 #if [ ! -e "stemcells/$BOSH_STEMCELL_FILE" ]; then
 #  curl -k $BOSH_STEMCELL_LOCATION/$BOSH_STEMCELL_FILE -o stemcells/$BOSH_STEMCELL_FILE
 #fi
-zip -r ${TILE_NAME}-${TILE_VERSION}.pivotal metadata releases #stemcells
+zip -r ${TILE_NAME}-${TILE_VERSION}.pivotal metadata releases content_migrations* #stemcells
 mv ${TILE_NAME}-${TILE_VERSION}.pivotal ..
 popd
